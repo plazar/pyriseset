975005408cc3e8cb6c92202fc6cf139dc56fcc59
Patrick Lazarus <patricklazarus@gmail.com>
2012-07-29 11:14:12 +0200

    Allow mode to be selected with regular expression.

--- modes/__init__.py
+++ modes/__init__.py
@@ -1,11 +1,23 @@
+import re
+
 registered_modes = ["graphics", \
                     "text", \
                    ]
 
 def run(modename, *args, **kwargs):
-    if modename in registered_modes:
-        mode = __import__(modename, globals())
+    matching_modes = [m for m in registered_modes \
+                        if re.search(modename, m, re.IGNORECASE)]
+
+    if len(matching_modes) == 1:
+        mode = __import__(matching_modes[0], globals())
         mode.run(*args, **kwargs)
-    else:
+    elif len(matching_modes) == 0:
         raise ValueError("Mode cannot be run. Name provided is not " \
                             "recognized: '%s'" % modename)
+    elif len(matching_modes) > 1:
+        raise ValueError("Mode name provided in ambiguous. The following " \
+                            "modes match: '%s'" % "', '".join(matching_modes))
+    else:
+        raise ValueError("Should not be here! Negative number of matching " \
+                            "modes? len(matching_modes)=%d; modename='%s'" % \
+                            len(matching_modes, modename))
