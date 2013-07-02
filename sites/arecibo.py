f62e4c81b0e371880c8a81fd020ce7f996b8ab67
Patrick Lazarus <plazarus@mpifr.de>
2013-07-02 18:02:39 +0200

    Slight update of AO coordinates.
    Now using astronomical coords.

--- sites/arecibo.py
+++ sites/arecibo.py
@@ -3,8 +3,10 @@ import utils
 
 class Arecibo(base.BaseSite):
     name = "Arecibo"
-    lon = utils.dmsstr_to_deg("-66:45:11.1")
-    lat = utils.dmsstr_to_deg("18:20:36.6")
+    #lon = utils.dmsstr_to_deg("-66:45:11.1")
+    #lat = utils.dmsstr_to_deg("18:20:36.6")
+    lon = utils.dmsstr_to_deg("-66:45:18.8")
+    lat = utils.dmsstr_to_deg("18:21:13.7")
     azspeed = 0.4*60 # deg/minute
     altspeed = 0.04*60 # deg/minute
 
